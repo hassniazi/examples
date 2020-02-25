de6bb3ad374131a3f56e9f73e0a4ba718f1bcc77
Hassan Niazi <hassanniazi@MacBook-Pro-3.local>
2020-02-25 14:55:40 +0000

    update dried python

--- DRY/dry-after.py
+++ DRY/dry-after.py
@@ -16,70 +16,49 @@ class MazeRequesterException(Exception):
 
 class MazeRequester:
     def __init__(self, data={}):
-      
       if not MAZE_URL:
         logging.debug('MAZE_URL not in {}'.format(os.environ.keys()))
         raise MazeRequesterException('Could not find Maze.')
       self.data = data
-      token = os.environ.get('BEARER_TOKEN') 
+      token = bearer_token()
       auth = 'Bearer {}'.format(token)
       self.headers = {
           'content-type': 'application/json',
           'authorization': auth
       }
-
-    def get(self,url):
+    
+    def __request(self, url, method='get'):
       full_url = MAZE_URL + url
-      logging.debug('Starting GET request to {}'.format(full_url))
+      logging.debug('Starting {} request to {}'.format(method.upper(), full_url))
       try:
-        response = requests.get(
-            full_url,
-            headers=self.headers,
-            verify=USE_HTTPS,
-            timeout=DEFAULT_TIMEOUT
+        request = getattr(requests, method.lower())
+        response = request(
+          full_url,
+          headers=self.headers,
+          verify=USE_HTTPS,
+          timeout=DEFAULT_TIMEOUT
         )
         if response.status_code >= 300:
-            self.error_handler(response)
+          self.error_handler(response)
         else:
           return response.json()
       except requests.exceptions.ReadTimeout:
         self.timeout(full_url)
+      except requests.exceptions.ConnectTimeout:
+        self.timeout(full_url)
+      except AttributeError:
+        raise MazeRequesterException(
+          'Invalid method {} provided to request'.format(method)
+        )
 
+    def get(self, url):
+      return self.__request(url, method='get')
+      
     def post(self, url):
-      full_url = MAZE_URL + url
-      logging.debug('Starting POST request to {}'.format(full_url))
-      try:
-        response = requests.post(
-            full_url,
-            headers=self.headers,
-            data = json.dumps(self.data),
-            verify=USE_HTTPS,
-            timeout=DEFAULT_TIMEOUT
-        )
-        if response.status_code >= 300:
-            self.error_handler(response)
-        else:
-          return response.json()
-      except requests.exceptions.ReadTimeout:
-        self.timeout(full_url)
+      return self.__request(url, method='post')
         
     def put(self, url):
-      full_url = MAZE_URL + url
-      logging.debug('Starting PUT request to {}'.format(full_url))
-      try:
-        response = requests.put(
-            full_url,
-            headers=self.headers,
-            data = json.dumps(self.data),
-            verify=USE_HTTPS,
-            timeout=DEFAULT_TIMEOUT
-        )
-        if response.status_code >= 300:
-            self.error_handler(response)
-        else:
-          return response.json()
-      except requests.exceptions.ReadTimeout:
-        self.timeout(full_url)
+      return self.__request(url, method='put')
 
     def error_handler(self,response):
       logging.warning('Response status = {}'.format(response.status_code))
